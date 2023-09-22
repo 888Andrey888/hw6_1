1389754cfaca2d73057d3b5772b781c66f4476a4
888Andrey888 <andrey.reshetoff.88@gmail.com>
2023-09-23 00:40:52 +0600

    hw6_2

--- app/src/main/java/com/example/hw6_1/presentation/addtask/AddTaskFragment.kt
+++ app/src/main/java/com/example/hw6_1/presentation/addtask/AddTaskFragment.kt
@@ -1,12 +1,10 @@
 package com.example.hw6_1.presentation.addtask
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.setFragmentResult
 import androidx.fragment.app.setFragmentResultListener
 import androidx.lifecycle.ViewModel
